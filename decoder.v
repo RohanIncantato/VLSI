f19be7ad5f3f26eaaab9cfde059d19f80a788731
Rohan  Safar <106857699+RohanIncantato@users.noreply.github.com>
2023-12-14 10:28:59 +0530

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -9,7 +9,7 @@ begin
 if(en==0)
 begin
 if(a==1'bo & b=='bo) y=4'bll1 1110;
-else if(a==1'b0 & b==1'bl) y=4'bl101;
+  else if(a==1'b0 & b==1'bl) y=4'b1101;
 else if(a==1'bl & b==1'bo 'bo) y y=4'b1011;
 else if(a==1 & b==1) y=4'boll1;
 else y=4'bxxxx;
