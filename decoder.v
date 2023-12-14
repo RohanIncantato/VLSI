0f44c02cee80d7b0cd71bd3dcbd6a1d2bd809c48
Rohan  Safar <106857699+RohanIncantato@users.noreply.github.com>
2023-12-14 10:30:54 +0530

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -8,13 +8,13 @@ always @ (en or a or b)
 begin
 if(en==0)
 begin
-if(a==1'bo & b=='bo) y=4'bll1 1110;
+if(a==1'bo & b=='bo) y=4'1110;
   else if(a==1'b0 & b==1'bl) y=4'b1101;
 else if(a==1'bl & b==1'bo 'bo) y y=4'b1011;
-else if(a==1 & b==1) y=4'boll1;
+  else if(a==1 & b==1) y=4'b0111;
 else y=4'bxxxx;
 end
 else
-y='blll1;
+y='b1111;
 end
 endmodule
